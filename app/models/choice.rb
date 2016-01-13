a3f074de9208d936b3734271350ed359719d28aa
Vito Cairone <vitocairone@gmail.com>
2016-01-12 16:58:53 -0800

    show result tallies in result view

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,4 +2,8 @@ class Choice < ActiveRecord::Base
   belongs_to :issue
   belongs_to :speaker
   has_many :voices
+
+	def to_s
+  	"... " + text[10..-1]
+  end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,10 @@
 class Issue < ActiveRecord::Base
   belongs_to :speaker
   has_many :choices
+  has_many :voices
   accepts_nested_attributes_for :choices, allow_destroy: true
+
+  def to_s
+  	codename
+  end
 end
