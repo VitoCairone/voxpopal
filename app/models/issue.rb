76066077a52fc90767e22148b80fc29b64357f64
Vito Cairone <vitocairone@gmail.com>
2016-01-14 22:16:20 -0800

    resize text boxes and refine style scope

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,4 +7,13 @@ class Issue < ActiveRecord::Base
   def to_s
   	codename
   end
+
+  def top(n, offset)
+  	# query the DB here
+  	#
+  	# note: MAKE SURE the database keeps an index of Issue by Boost.
+  	# That way, this query performs O(log m) where m is the db size.
+  	# Without an index it is O(m log m) or potentially worse.
+  	self.order_by(:boost).limit(n, offset)
+  end
 end
